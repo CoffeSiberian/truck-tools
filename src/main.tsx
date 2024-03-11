e122899a03502991a7296f9b08a651c64839f546
Fernando Garrido <siberiancoffe@outlook.cl>
2024-03-10 22:38:25 -0300

    Update HTML and main.tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <NextUIProvider>
             <DarkMode>
-                <main className="dark text-foreground bg-background min-h-screen">
-                    <App />
-                </main>
+                <App />
             </DarkMode>
         </NextUIProvider>
     </React.StrictMode>
