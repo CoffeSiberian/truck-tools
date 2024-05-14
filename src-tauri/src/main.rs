45695f758ae87bb71bef954ba352403198d073b5
Fernando Garrido <siberiancoffe@outlook.cl>
2024-05-14 14:00:18 -0400

    code improvements: separation of functions for trailers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,138 +1,150 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod structs;
+mod utils;
+mod main_options;
+use utils::file_edit::{
+    read_file_text, 
+    save_file, 
+    get_list_save_game, 
+    get_list_save_count
+};
 use serde_json::json;
 use structs::vec_save_games::VecSaveGames;
-mod utils;
-mod structs;
+use main_options::trailers::{
+    get_my_trailer_id, 
+    get_trailer_index, 
+    set_cargo_mass_trailer, 
+    get_slave_trailers_id, 
+    set_any_slave_trailers_weight, 
+    get_trailer_def_id, 
+    get_trailer_def_index, 
+    set_chassis_and_body_mass_def_trailers, 
+    set_remove_trailer_restricted_areas
+};
 
 const RESPONSE_FALSE: &str = r#"{"res": false}"#;
 const RESPONSE_TRUE: &str = r#"{"res": true}"#;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[tauri::command]
 fn set_cargo_mass_trailers_and_slave(cargo_mass: &str, dir_save: &str) -> String {    
     // 100 ms
-    let file: Vec<String> = match utils::file_edit::read_file_text(dir_save) {
+    let file: Vec<String> = match read_file_text(dir_save) {
         Some(file) => file,
         None => return RESPONSE_FALSE.to_string(),
     };
 
     // 25 ms
-    let trailer_id: String = match utils::file_edit::get_my_trailer_id(&file) {
+    let trailer_id: String = match get_my_trailer_id(&file) {
         Some(trailer_id) => trailer_id,
         None => return RESPONSE_FALSE.to_string(),
     };
     
     // 25 ms
-    let trailer_index: usize = match utils::file_edit::get_trailer_index(&file, trailer_id) {
+    let trailer_index: usize = match get_trailer_index(&file, trailer_id) {
         Some(trailer_index) => trailer_index,
         None => return RESPONSE_FALSE.to_string(),
     };
     
     // 30 ms
-    let cargo_mass_save: Vec<String> = match utils::file_edit::set_cargo_mass_trailer(&file, trailer_index, cargo_mass) {
+    let cargo_mass_save: Vec<String> = match set_cargo_mass_trailer(&file, trailer_index, cargo_mass) {
         Some(cargo_mass_save) => cargo_mass_save,
         None => return RESPONSE_FALSE.to_string(),
     };
 
     // 0 ms
-    let slave_trailer_id: String = match utils::file_edit::get_slave_trailers_id(&file, trailer_index) {
+    let slave_trailer_id: String = match get_slave_trailers_id(&file, trailer_index) {
         Some(slave_trailer_id) => slave_trailer_id,
         None => {
-            utils::file_edit::save_file(dir_save.to_string(), cargo_mass_save);
+            save_file(dir_save.to_string(), cargo_mass_save);
             return RESPONSE_TRUE.to_string();
         },
     };
     
     // 185 ms
-    let cargo_mass_save_slave: Vec<String> = utils::file_edit::set_any_slave_trailers_weight(&cargo_mass_save, slave_trailer_id, cargo_mass.to_string());
+    let cargo_mass_save_slave: Vec<String> = set_any_slave_trailers_weight(&cargo_mass_save, slave_trailer_id, cargo_mass.to_string());
     
     // 60 ms
-    utils::file_edit::save_file(dir_save.to_string(), cargo_mass_save_slave);
+    save_file(dir_save.to_string(), cargo_mass_save_slave);
     return RESPONSE_TRUE.to_string();
 }
 
 #[tauri::command]
 fn set_unlock_current_trailers(dir_save: &str) -> String {
-    let file: Vec<String> = match utils::file_edit::read_file_text(dir_save) {
+    let file: Vec<String> = match read_file_text(dir_save) {
         Some(file) => file,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_id: String = match utils::file_edit::get_my_trailer_id(&file) {
+    let trailer_id: String = match get_my_trailer_id(&file) {
         Some(trailer_id) => trailer_id,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_index: usize = match utils::file_edit::get_trailer_index(&file, trailer_id) {
+    let trailer_index: usize = match get_trailer_index(&file, trailer_id) {
         Some(trailer_index) => trailer_index,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_def_id: String = match utils::file_edit::get_trailer_def_id(&file, trailer_index) {
+    let trailer_def_id: String = match get_trailer_def_id(&file, trailer_index) {
         Some(trailer_def_id) => trailer_def_id,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_def_index: usize = match utils::file_edit::get_trailer_def_index(&file, trailer_def_id) {
+    let trailer_def_index: usize = match get_trailer_def_index(&file, trailer_def_id) {
         Some(trailer_def_index) => trailer_def_index,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_unlocked: Vec<String> = match utils::file_edit::set_remove_trailer_restricted_areas(&file, trailer_def_index) {
+    let trailer_unlocked: Vec<String> = match set_remove_trailer_restricted_areas(&file, trailer_def_index) {
         Some(trailer_unlocked) => trailer_unlocked,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    utils::file_edit::save_file(dir_save.to_string(), trailer_unlocked);
+    save_file(dir_save.to_string(), trailer_unlocked);
     return RESPONSE_TRUE.to_string();
 }
 
 #[tauri::command]
 fn set_cargo_mass_def_trailers(dir_save: &str, body_mass: &str, chassis_mass: &str) -> String {
-    let file: Vec<String> = match utils::file_edit::read_file_text(dir_save) {
+    let file: Vec<String> = match read_file_text(dir_save) {
         Some(file) => file,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_id: String = match utils::file_edit::get_my_trailer_id(&file) {
+    let trailer_id: String = match get_my_trailer_id(&file) {
         Some(trailer_id) => trailer_id,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_index: usize = match utils::file_edit::get_trailer_index(&file, trailer_id) {
+    let trailer_index: usize = match get_trailer_index(&file, trailer_id) {
         Some(trailer_index) => trailer_index,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_def_id: String = match utils::file_edit::get_trailer_def_id(&file, trailer_index) {
+    let trailer_def_id: String = match get_trailer_def_id(&file, trailer_index) {
         Some(trailer_def_id) => trailer_def_id,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let trailer_def_index: usize = match utils::file_edit::get_trailer_def_index(&file, trailer_def_id) {
+    let trailer_def_index: usize = match get_trailer_def_index(&file, trailer_def_id) {
         Some(trailer_def_index) => trailer_def_index,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    let chassis_and_body_mass = match utils::file_edit::set_chassis_and_body_mass_def_trailers(&file, trailer_def_index, body_mass, chassis_mass) {
+    let chassis_and_body_mass = match set_chassis_and_body_mass_def_trailers(&file, trailer_def_index, body_mass, chassis_mass) {
         Some(chassis_mass_and_body_mass) => chassis_mass_and_body_mass,
         None => return RESPONSE_FALSE.to_string(),
     };
 
-    utils::file_edit::save_file(dir_save.to_string(), chassis_and_body_mass);
+    save_file(dir_save.to_string(), chassis_and_body_mass);
     return RESPONSE_TRUE.to_string();
 }
 
 #[tauri::command]
 async fn get_save_game_name(dir_save: &str) -> Result<String, ()> {
-    let mut save_games_name: Vec<VecSaveGames> = match utils::file_edit::get_list_save_game(dir_save.to_string()).await {
+    let mut save_games_name: Vec<VecSaveGames> = match get_list_save_game(dir_save.to_string()).await {
         Some(save_games_name) => save_games_name,
         None => return Ok(RESPONSE_FALSE.to_string()),
     };
@@ -148,7 +160,7 @@ async fn get_save_game_name(dir_save: &str) -> Result<String, ()> {
 
 #[tauri::command]
 fn get_save_game_count(dir_save: &str) -> String {
-    let save_games: usize = utils::file_edit::get_list_save_count(dir_save.to_string());
+    let save_games: usize = get_list_save_count(dir_save.to_string());
 
     let response: String = json!({
         "res": true,
@@ -160,7 +172,13 @@ fn get_save_game_count(dir_save: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, set_cargo_mass_trailers_and_slave, set_unlock_current_trailers, set_cargo_mass_def_trailers, get_save_game_name, get_save_game_count])
+        .invoke_handler(tauri::generate_handler![
+            set_cargo_mass_trailers_and_slave, 
+            set_unlock_current_trailers, 
+            set_cargo_mass_def_trailers, 
+            get_save_game_name, 
+            get_save_game_count
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
