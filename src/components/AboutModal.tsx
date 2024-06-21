ba7ef29e6dab623d590406fcfb0f104f6654b0e6
Fernando Garrido <siberiancoffe@outlook.cl>
2024-06-21 15:31:22 -0400

    chore: Bump version to 0.1.2

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -36,7 +36,7 @@ const AboutModal: FC<AboutModalProps> = ({ isOpen, onOpenChange }) => {
 					{(_onClose) => (
 						<>
 							<ModalHeader className="flex flex-col gap-1">
-								Truck Tools - Preview 0.1.1
+								Truck Tools - Preview 0.1.2
 							</ModalHeader>
 							<Divider />
 							<ModalBody className="flex items-center justify-center py-1">
