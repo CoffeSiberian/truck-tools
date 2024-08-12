5cf5851d8a9fa4fb5ac6d4c986e2708b5b01972b
Elian Webster <elianwebster0@gmail.com>
2024-08-12 10:27:30 -0500

    add DAF, MAN, Mercedes, Iveco and Renault engines

---
+++ src-tauri/src/main_options/trucks_engines_data/daf.rs
@@ -0,0 +1,119 @@
+use super::super::super::structs::vec_trucks::EngineStruct;
+
+/*_____               ______    __   __  ______ 
+|  __ \      /\     |  ____|   \ \ / / |  ____|
+| |  | |    /  \    | |__       \ V /  | |__   
+| |  | |   / /\ \   |  __|       > <   |  __|  
+| |__| |  / ____ \  | |         / . \  | |     
+|_____/  /_/    \_\ |_|        /_/ \_\ |_|     
+*/
+
+pub const DAF_XF_MX265_360: EngineStruct = EngineStruct {
+    name_id: "mx265.daf.xf.engine",
+    name: "PACCAR MX265",
+    cv: "360",
+    nm: "1775",
+    code: "/def/vehicle/truck/daf.xf/engine/mx265.sii",
+};
+
+pub const DAF_XF_MX300_410: EngineStruct = EngineStruct {
+    name_id: "mx300.daf.xf.engine",
+    name: "PACCAR MX300",
+    cv: "410",
+    nm: "2000",
+    code: "/def/vehicle/truck/daf.xf/engine/mx300.sii",
+};
+
+pub const DAF_XF_MX340_460: EngineStruct = EngineStruct {
+    name_id: "mx340.daf.xf.engine",
+    name: "PACCAR MX340",
+    cv: "460",
+    nm: "2300",
+    code: "/def/vehicle/truck/daf.xf/engine/mx340.sii",
+};
+
+pub const DAF_XF_MX375_510: EngineStruct = EngineStruct {
+    name_id: "mx375.daf.xf.engine",
+    name: "PACCAR MX375",
+    cv: "510",
+    nm: "2500",
+    code: "/def/vehicle/truck/daf.xf/engine/mx375.sii",
+};
+
+/*_____               ______    __   __  ______ 
+|  __ \      /\     |  ____|   \ \ / / |  ____|
+| |  | |    /  \    | |__       \ V /  | |__   
+| |  | |   / /\ \   |  __|       > <   |  __|  
+| |__| |  / ____ \  | |         / . \  | |     
+|_____/  /_/    \_\ |_|        /_/ \_\ |_|
+______   _    _   _____     ____        __  
+|  ____| | |  | | |  __ \   / __ \      / /  
+| |__    | |  | | | |__) | | |  | |    / /_  
+|  __|   | |  | | |  _  /  | |  | |   | '_ \ 
+| |____  | |__| | | | \ \  | |__| |   | (_) |
+|______|  \____/  |_|  \_\  \____/     \___/      
+*/
+
+pub const DAF_XF_EU6_MX11_370: EngineStruct = EngineStruct {
+    name_id: "mx11_270.daf.xf_euro6.engine",
+    name: "MX-11 270 Euro 6, 2017",
+    cv: "370",
+    nm: "1900",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx11_270.sii",
+};
+
+pub const DAF_XF_EU6_MX11_435: EngineStruct = EngineStruct {
+    name_id: "mx11_320.daf.xf_euro6.engine",
+    name: "MX-11 320 Euro 6",
+    cv: "435",
+    nm: "2100",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx11_320.sii",
+};
+
+pub const DAF_XF_EU6_MX11_450: EngineStruct = EngineStruct {
+    name_id: "mx11_330.daf.xf_euro6.engine",
+    name: "MX-11 330 Euro 6, 2017",
+    cv: "450",
+    nm: "2200",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx11_330.sii",
+};
+
+pub const DAF_XF_EU6_MX13_410: EngineStruct = EngineStruct {
+    name_id: "mx13_303.daf.xf_euro6.engine",
+    name: "MX-13 303 Euro 6",
+    cv: "410",
+    nm: "2000",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_303.sii",
+};
+
+pub const DAF_XF_EU6_MX13_460: EngineStruct = EngineStruct {
+    name_id: "mx13_340.daf.xf_euro6.engine",
+    name: "MX-13 340 Euro 6",
+    cv: "460",
+    nm: "2300",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_340.sii",
+};
+
+pub const DAF_XF_EU6_MX13_480: EngineStruct = EngineStruct {
+    name_id: "mx13_355.daf.xf_euro6.engine",
+    name: "MX-13 355 Euro 6, 2017",
+    cv: "480",
+    nm: "2350",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_355.sii",
+};
+
+pub const DAF_XF_EU6_MX13_510: EngineStruct = EngineStruct {
+    name_id: "mx13_375.daf.xf_euro6.engine",
+    name: "MX-13 375 Euro 6",
+    cv: "510",
+    nm: "2500",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_375.sii",
+};
+
+pub const DAF_XF_EU6_MX13_530: EngineStruct = EngineStruct {
+    name_id: "mx13_390.daf.xf_euro6.engine",
+    name: "MX-13 390 Euro 6, 2017",
+    cv: "530",
+    nm: "2500",
+    code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_390.sii",
+};
---
+++ src-tauri/src/main_options/trucks_engines_data/mercedes.rs
@@ -0,0 +1,148 @@
+use super::super::super::structs::vec_trucks::EngineStruct;
+
+/*__  __   ______   _____     _____   ______   _____    ______    _____                 _____   _______   _____     ____     _____ 
+|  \/  | |  ____| |  __ \   / ____| |  ____| |  __ \  |  ____|  / ____|       /\      / ____| |__   __| |  __ \   / __ \   / ____|
+| \  / | | |__    | |__) | | |      | |__    | |  | | | |__    | (___        /  \    | |         | |    | |__) | | |  | | | (___  
+| |\/| | |  __|   |  _  /  | |      |  __|   | |  | | |  __|    \___ \      / /\ \   | |         | |    |  _  /  | |  | |  \___ \ 
+| |  | | | |____  | | \ \  | |____  | |____  | |__| | | |____   ____) |    / ____ \  | |____     | |    | | \ \  | |__| |  ____) |
+|_|  |_| |______| |_|  \_\  \_____| |______| |_____/  |______| |_____/    /_/    \_\  \_____|    |_|    |_|  \_\  \____/  |_____/ 
+*/
+
+
+pub const MERCEDES_ACTROS_1832LS: EngineStruct = EngineStruct {
+    name_id: "1832ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 235",
+    cv: "320",
+    nm: "1650",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1832ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1836LS: EngineStruct = EngineStruct {
+    name_id: "1836ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 265",
+    cv: "360",
+    nm: "1850",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1836ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1841LS: EngineStruct = EngineStruct {
+    name_id: "1841ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 300",
+    cv: "408",
+    nm: "2000",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1841ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1844LS: EngineStruct = EngineStruct {
+    name_id: "1844ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 320",
+    cv: "435",
+    nm: "2100",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1844ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1846LS: EngineStruct = EngineStruct {
+    name_id: "1846ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 335",
+    cv: "456",
+    nm: "2200",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1846ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1848LS: EngineStruct = EngineStruct {
+    name_id: "1848ls.mercedes.actros.engine",
+    name: "OM 541 V6 BlueTec5 350",
+    cv: "476",
+    nm: "2300",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1848ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1851LS: EngineStruct = EngineStruct {
+    name_id: "1851ls.mercedes.actros.engine",
+    name: "OM 542 V8 BlueTec5 375",
+    cv: "510",
+    nm: "2400",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1851ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1855LS: EngineStruct = EngineStruct {
+    name_id: "1855ls.mercedes.actros.engine",
+    name: "OM 542 V8 BlueTec5 405",
+    cv: "551",
+    nm: "2600",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1855ls.sii",
+};
+
+pub const MERCEDES_ACTROS_1860LS: EngineStruct = EngineStruct {
+    name_id: "1860ls.mercedes.actros.engine",
+    name: "OM 542 V8 BlueTec5 440",
+    cv: "598",
+    nm: "2800",
+    code: "/def/vehicle/truck/mercedes.actros/engine/1860ls.sii",
+};
+
+/*__  __   ______   _____     _____   ______   _____    ______    _____     _   _   ______  __          __                _____   _______   _____     ____     _____ 
+|  \/  | |  ____| |  __ \   / ____| |  ____| |  __ \  |  ____|  / ____|   | \ | | |  ____| \ \        / /       /\      / ____| |__   __| |  __ \   / __ \   / ____|
+| \  / | | |__    | |__) | | |      | |__    | |  | | | |__    | (___     |  \| | | |__     \ \  /\  / /       /  \    | |         | |    | |__) | | |  | | | (___  
+| |\/| | |  __|   |  _  /  | |      |  __|   | |  | | |  __|    \___ \    | . ` | |  __|     \ \/  \/ /       / /\ \   | |         | |    |  _  /  | |  | |  \___ \ 
+| |  | | | |____  | | \ \  | |____  | |____  | |__| | | |____   ____) |   | |\  | | |____     \  /\  /       / ____ \  | |____     | |    | | \ \  | |__| |  ____) |
+|_|  |_| |______| |_|  \_\  \_____| |______| |_____/  |______| |_____/    |_| \_| |______|     \/  \/       /_/    \_\  \_____|    |_|    |_|  \_\  \____/  |_____/ 
+*/
+
+pub const MERCEDES_NEW_ACTROS_1842: EngineStruct = EngineStruct {
+    name_id: "engine42.mercedes.actros2014.engine",
+    name: "OM 471 Euro VI 310",
+    cv: "421",
+    nm: "2100",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine_1842.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1845: EngineStruct = EngineStruct {
+    name_id: "engine45.mercedes.actros2014.engine",
+    name: "OM 471 Euro VI 330",
+    cv: "449",
+    nm: "2200",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine_1845.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1848: EngineStruct = EngineStruct {
+    name_id: "engine48.mercedes.actros2014.engine",
+    name: "OM 471 Euro VI 350",
+    cv: "476",
+    nm: "2300",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine/engine_1848.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1851: EngineStruct = EngineStruct {
+    name_id: "engine51.mercedes.actros2014.engine",
+    name: "OM 471 Euro VI 375",
+    cv: "510",
+    nm: "2500",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine/engine_1851.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1852: EngineStruct = EngineStruct {
+    name_id: "engine52.mercedes.actros2014.engine",
+    name: "OM 473 Euro VI 380",
+    cv: "517",
+    nm: "2600",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine/engine_1852.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1858: EngineStruct = EngineStruct {
+    name_id: "engine58.mercedes.actros2014.engine",
+    name: "OM 473 Euro VI 425",
+    cv: "578",
+    nm: "2800",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine/engine_1858.sii",
+};
+
+pub const MERCEDES_NEW_ACTROS_1863: EngineStruct = EngineStruct {
+    name_id: "engine63.mercedes.actros2014.engine",
+    name: "OM 473 Euro VI 460",
+    cv: "625",
+    nm: "3000",
+    code: "/def/vehicle/truck/mercedes.actros2014/engine/engine_1863.sii",
+};
+
+
