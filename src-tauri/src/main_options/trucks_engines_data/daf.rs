9b3242d087f6c9e4baf8cac47a6e6139d39d6f41
Elian Webster <elianwebster0@gmail.com>
2024-08-12 23:05:17 -0500

    added DAF 2021 engines

--- src-tauri/src/main_options/trucks_engines_data/daf.rs
+++ src-tauri/src/main_options/trucks_engines_data/daf.rs
@@ -117,3 +117,65 @@ pub const DAF_XF_EU6_MX13_530: EngineStruct = EngineStruct {
     nm: "2500",
     code: "/def/vehicle/truck/daf.xf_euro6/engine/mx13_390.sii",
 };
+
+/*_____               ______  
+|  __ \      /\     |  ____|  
+| |  | |    /  \    | |__    
+| |  | |   / /\ \   |  __|   
+| |__| |  / ____ \  | |       
+|_____/  /_/    \_\ |_|           
+  ___     ___    ___    __ 
+ |__ \   / _ \  |__ \  /_ |
+    ) | | | | |    ) |  | |
+   / /  | | | |   / /   | |
+  / /_  | |_| |  / /_   | |
+ |____|  \___/  |____|  |_|
+*/
+
+pub const DAF_2021_MX11_367: EngineStruct = EngineStruct {
+    name_id: "mx11_270.daf.2021.engine",
+    name: "MX-11 270",
+    cv: "367",
+    nm: "1800 - 1950",
+    code: "/def/vehicle/truck/daf.2021/engine/mx11_270.sii",
+};
+
+pub const DAF_2021_MX11_408: EngineStruct = EngineStruct {
+    name_id: "mx11_300.daf.2021.engine",
+    name: "MX-11 300",
+    cv: "408",
+    nm: "2000 - 2150",
+    code: "/def/vehicle/truck/daf.2021/engine/mx11_300.sii",
+};
+
+pub const DAF_2021_MX11_449: EngineStruct = EngineStruct {
+    name_id: "mx11_330.daf.2021.engine",
+    name: "MX-11 330",
+    cv: "449",
+    nm: "2200 - 2350",
+    code: "/def/vehicle/truck/daf.2021/engine/mx11_330.sii",
+};
+
+pub const DAF_2021_MX13_428: EngineStruct = EngineStruct {
+    name_id: "mx13_315.daf.2021.engine",
+    name: "MX-13 315",
+    cv: "428",
+    nm: "2150 - 2300",
+    code: "/def/vehicle/truck/daf.2021/engine/mx13_315.sii",
+};
+
+pub const DAF_2021_MX13_483: EngineStruct = EngineStruct {
+    name_id: "mx13_355.daf.2021.engine",
+    name: "MX-13 355",
+    cv: "483",
+    nm: "2350 - 2500",
+    code: "/def/vehicle/truck/daf.2021/engine/mx13_355.sii",
+};
+
+pub const DAF_2021_MX13_530: EngineStruct = EngineStruct {
+    name_id: "mx13_390.daf.2021.engine",
+    name: "MX-13 390",
+    cv: "530",
+    nm: "2550 - 2700",
+    code: "/def/vehicle/truck/daf.2021/engine/mx13_390.sii",
+};
