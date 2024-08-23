7b26e95420fd7d8c0804d4bb215eb709bd36bffe
Fernando Garrido <siberiancoffe@outlook.cl>
2024-08-23 03:47:55 -0400

    feat: Improved the styling of the settings modal

--- src/components/Modals/SettingsModal.tsx
+++ src/components/Modals/SettingsModal.tsx
@@ -45,40 +45,37 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, onOpenChange }) => {
 						<ModalHeader className="flex flex-col gap-1">Settings</ModalHeader>
 						<Divider />
 						<ModalBody className="flex gap-3 pb-1">
-							<div className="flex flex-col gap-1">
+							<div className="flex justify-center gap-1">
 								<Select
 									selectedKeys={[userTheme]}
 									onChange={(e) => onClickTheme(e.target.value as any)}
 									label="Change theme"
 									variant="bordered"
 									placeholder="Select a theme"
-									className="max-w-xs"
 								>
 									<SelectItem key="system">System</SelectItem>
 									<SelectItem key="dark">Dark</SelectItem>
 									<SelectItem key="light">Light </SelectItem>
 								</Select>
 							</div>
-							<div className="flex flex-col gap-1">
+							<div className="flex justify-center gap-1">
 								<Select
 									label="Change language"
 									variant="bordered"
 									placeholder="Select a language"
-									className="max-w-xs"
 									isDisabled
 								>
 									<SelectItem key="light ">English</SelectItem>
 								</Select>
 							</div>
-							<div className="flex flex-col gap-1">
+							<div className="flex gap-1">
 								<Switch isDisabled>Enable console and developer mode</Switch>
 							</div>
-							<div className="flex flex-col gap-1">
+							<div className="flex gap-1">
 								<Switch isDisabled>Enable 128 convoy mode slots</Switch>
 							</div>
-							<div className="flex flex-col gap-1">
+							<div className="flex gap-1">
 								<Input
-									className="mt-1"
 									disabled={true}
 									startContent={<IconFileTypeZip />}
 									endContent={
