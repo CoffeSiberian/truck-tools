f973ff6960a770bc0ed6e3f604022522468f3b6f
Fernando Garrido <siberiancoffe@outlook.cl>
2024-08-23 16:34:25 -0400

    Options without implemented functionality hidden

--- src/components/Modals/SettingsModal.tsx
+++ src/components/Modals/SettingsModal.tsx
@@ -58,37 +58,46 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, onOpenChange }) => {
 									<SelectItem key="light">Light </SelectItem>
 								</Select>
 							</div>
-							<div className="flex justify-center gap-1">
-								<Select
-									label="Change language"
-									variant="bordered"
-									placeholder="Select a language"
-									isDisabled
-								>
-									<SelectItem key="light ">English</SelectItem>
-								</Select>
-							</div>
-							<div className="flex gap-1">
-								<Switch isDisabled>Enable console and developer mode</Switch>
-							</div>
-							<div className="flex gap-1">
-								<Switch isDisabled>Enable 128 convoy mode slots</Switch>
-							</div>
-							<div className="flex gap-1">
-								<Input
-									disabled={true}
-									startContent={<IconFileTypeZip />}
-									endContent={
-										<Button
-											color="primary"
-											startContent={<IconFolderSearch />}
+							<div className="relative">
+								<div className="absolute inset-0 flex items-center justify-center">
+									<p className="font-bold">Soon ☕</p>
+								</div>
+								<div className="blur-md">
+									<div className="flex justify-center gap-1">
+										<Select
+											label="Change language"
+											variant="bordered"
+											placeholder="Select a language"
+											isDisabled
+										>
+											<SelectItem key="light ">English</SelectItem>
+										</Select>
+									</div>
+									<div className="flex">
+										<Switch isDisabled>
+											Enable console and developer mode
+										</Switch>
+									</div>
+									<div className="flex">
+										<Switch isDisabled>Enable 128 convoy mode slots</Switch>
+									</div>
+									<div className="flex">
+										<Input
 											disabled={true}
+											startContent={<IconFileTypeZip />}
+											endContent={
+												<Button
+													color="primary"
+													startContent={<IconFolderSearch />}
+													disabled={true}
+												/>
+											}
+											label="Document folder"
+											placeholder="Enter the document folder"
+											isDisabled
 										/>
-									}
-									label="Document folder"
-									placeholder="Enter the document folder"
-									isDisabled
-								/>
+									</div>
+								</div>
 							</div>
 						</ModalBody>
 						<ModalFooter className="items-center justify-center gap-1"></ModalFooter>
