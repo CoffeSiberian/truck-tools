9f9bd535552cb1efadafc3ac882ab25725136559
Fernando Garrido <siberiancoffe@outlook.cl>
2024-10-23 19:35:30 -0300

    Refactor import path for UpdaterModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useContext } from "react";
 import { DarkModeContex } from "@/hooks/useDarkModeContex";
 import Home from "@/routes/pages/Home";
-import UpdaterModal from "@/components/UpdaterModal";
+import UpdaterModal from "@/components/Modals/UpdaterModal";
 import SelectProfile from "@/components/ProfileMenu/SelectProfile";
 
 const App = () => {
