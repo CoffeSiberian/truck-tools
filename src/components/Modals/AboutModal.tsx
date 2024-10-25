ffbd01a707723686055be5f5d1a0e1a23bf1cfd4
Fernando Garrido <siberiancoffe@outlook.cl>
2024-10-25 18:27:30 -0300

    chore: Update version to 0.5.0

--- src/components/Modals/AboutModal.tsx
+++ src/components/Modals/AboutModal.tsx
@@ -36,7 +36,7 @@ const AboutModal: FC<AboutModalProps> = ({ isOpen, onOpenChange }) => {
 				{() => (
 					<>
 						<ModalHeader className="flex flex-col gap-1">
-							Truck Tools - Preview 0.4.9
+							Truck Tools - Preview 0.5.0
 						</ModalHeader>
 						<Divider />
 						<ModalBody className="flex items-center justify-center py-1">
