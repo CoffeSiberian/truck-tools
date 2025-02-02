e0099817ddd55302f00f7079ea8bf6f374012469
Fernando Garrido <siberiancoffe@outlook.cl>
2025-02-02 18:09:38 -0300

    Refactor App component by removing unused Snowfall feature and related logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { useEffect, useContext, useRef } from "react";
+import { useEffect, useContext } from "react";
 import { DarkModeContex } from "@/hooks/useDarkModeContex";
 import Home from "@/routes/pages/Home";
-import Snowfall from "react-snowfall";
 import UpdaterModal from "@/components/Modals/UpdaterModal";
 import SelectProfile from "@/components/ProfileMenu/SelectProfile";
 
+/*
+import Snowfall from "react-snowfall";
+
+const isHolidaysMonths = (): boolean => {
+	const month = new Date().getMonth();
+	return month === 11 || month === 0;
+};
+
+{isHolidaysMonth.current && (
+	<Snowfall
+		snowflakeCount={80}
+		color={!darkMode ? "#99d5e6" : undefined}
+	/>
+)}
+*/
+
+const defaultClassNames = "scrollbar bg-background text-foreground";
+
 const App = () => {
 	const { darkMode } = useContext(DarkModeContex);
-	const isHolidaysMonth = useRef(false);
-
-	const defaultClassNames = "scrollbar bg-background text-foreground";
-
-	const isHolidaysMonths = (): boolean => {
-		const month = new Date().getMonth();
-		return month === 11 || month === 0;
-	};
 
 	useEffect(() => {
 		const darkStyleSheets = [
 			"/md-light-indigo/theme.css",
 			"/md-dark-indigo/theme.css",
 		];
-
-		isHolidaysMonth.current = isHolidaysMonths();
 
 		document.body.className = darkMode
 			? `${defaultClassNames} dark`
@@ -45,12 +52,6 @@ const App = () => {
 		<div className="relative flex flex-col">
 			<Home />
 			<SelectProfile />
-			{isHolidaysMonth && (
-				<Snowfall
-					snowflakeCount={80}
-					color={!darkMode ? "#99d5e6" : undefined}
-				/>
-			)}
 			<UpdaterModal />
 		</div>
 	);
