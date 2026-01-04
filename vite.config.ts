0a4438931ce0f5b5352e8e7258482f390b3670f1
Fernando Garrido <siberiancoffe@outlook.cl>
2026-01-04 20:08:20 -0300

    Add babel-plugin-react-compiler to dependencies and configure it in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+		tailwindcss(),
+	],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
