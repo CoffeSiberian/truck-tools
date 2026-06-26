9a091fe9e9f4a42b3650ddfd19e200d5cf9f1593
rpastuhov <roman.pastuhov12200+github@gmail.com>
2026-06-26 16:12:56 +0200

    Fix set_player_trailer_file

--- src-tauri/src/main_options/trailers.rs
+++ src-tauri/src/main_options/trailers.rs
@@ -404,34 +404,17 @@ pub fn get_list_trailers_info(arr_val: &Vec<String>) -> Option<Vec<VecSaveTraile
 }
 
 pub fn set_player_trailer_file(
-    arr_val: &Vec<String>,
-    truck_id: &str,
+    arr_val: &[String],
+    trailer_id: &str,
 ) -> Option<(Vec<VecItemsFind>, usize)> {
-    let mut vec_items_replace: Vec<VecItemsFind> = Vec::new();
+    let (_, index) = find_in_player_vehicles_block(arr_val, "trailer:")?;
 
-    let mut found_trailer: bool = false;
-    let mut trailer_index: usize = 0;
-    for (i, item) in arr_val.iter().enumerate() {
-        if item.contains("assigned_trailer") {
-            vec_items_replace.push(VecItemsFind {
-                index: i,
-                value: format!(" assigned_trailer: {}", truck_id),
-            });
-            vec_items_replace.push(VecItemsFind {
-                index: i + 1,
-                value: format!(" my_trailer: {}", truck_id),
-            });
-            found_trailer = true;
-            trailer_index = i;
-            break;
-        }
-    }
+    let replacement = vec![VecItemsFind {
+        index,
+        value: format!(" trailer: {}", trailer_id),
+    }];
 
-    if found_trailer {
-        return Some((vec_items_replace, trailer_index));
-    }
-
-    return None;
+    Some((replacement, index))
 }
 
 pub fn set_cargo_mass_trailer(
